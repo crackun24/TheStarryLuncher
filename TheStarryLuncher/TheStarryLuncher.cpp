be820d8eff73bf9977cba8e92f3f86d4c52c0793
crackun24 <53938436+crackun24@users.noreply.github.com>
2022-08-14 16:25:57 +0800

    获取系统变量

--- TheStarryLuncher/TheStarryLuncher.cpp
+++ TheStarryLuncher/TheStarryLuncher.cpp
@@ -6,6 +6,32 @@ TheStarryLuncher::TheStarryLuncher(QWidget *parent)
     ui.setupUi(this);
     setFixedSize(this->width(), this->height());//固定窗体大小
 
+    vector<string>test = getJavaPaths();
+
+    for(int i = 0;i < test.size();i++)
+    {
+        ui.test->append(test.at(i).c_str());
+    }
+}
+
+vector<string> TheStarryLuncher::getJavaPaths()
+{
+    vector<string> javaPaths;
+    string paths = getenv("PATH");
+    while (true)
+    {
+        auto pos = paths.find(';');
+        if(pos != string::npos)
+        {
+            javaPaths.push_back(paths.substr(0,pos + 1));
+            paths.erase(paths.begin(), paths.begin() + pos + 1);
+
+        }else
+        {
+            break;
+        }
+    }
+    return javaPaths;
 }
 
 TheStarryLuncher::~TheStarryLuncher()
--- TheStarryLuncher/TheStarryLuncher.h
+++ TheStarryLuncher/TheStarryLuncher.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #include <QtWidgets/QWidget>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
 #include "ui_TheStarryLuncher.h"
 
+using namespace std;
 class TheStarryLuncher : public QWidget
 {
     Q_OBJECT
 
 public:
     TheStarryLuncher(QWidget *parent = nullptr);
+    vector<string> getJavaPaths();//TODO 获取java的路径列表
     ~TheStarryLuncher();
 
 private:
